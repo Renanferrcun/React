437f39dc63c89dc9246c2067b417b5933b738753
Renan <renanfcunha@gmail.com>
2026-07-06 23:54:19 -0300

    Ajustes Finais e Deploy

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -22,13 +22,13 @@ function Footer() {
                     </p>
                     <p className='text-lg'>Acesse nossas redes sociais</p>
                     <div className='flex gap-2'>
-                        <a href="https://www.linkedin.com/in/seu_usuario" target="_blank">
+                        <a href="https://www.linkedin.com/" target="_blank">
                             <LinkedinLogoIcon size={48} weight='bold' />
                         </a>
-                        <a href="https://www.instagram.com/seu_usuario" target="_blank">
+                        <a href="https://www.instagram.com/" target="_blank">
                             <InstagramLogoIcon size={48} weight='bold' />
                         </a>
-                        <a href="https://www.facebook.com/seu_usuario" target="_blank">
+                        <a href="https://www.facebook.com/" target="_blank">
                             <FacebookLogoIcon size={48} weight='bold' />
                         </a>
                     </div>
